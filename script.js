5c3203faf7200471f0700ababada8fcaf45b527c
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-02-05 15:18:26 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// place script here
+// Hamburger menu
+	$("nav").css("right", (-1*$("nav").width()) + "px");
+	$("#nav-hamburger").click(function(){
+		if ($(this).hasClass("animcomplete")) {
+			$(this).removeClass("animcomplete");
+			$(this).addClass("closed");
+			$("nav").css("right", (-1*$("nav").width()) + "px");
+			setTimeout(function() {
+				$("#nav-hamburger").removeClass("closed");
+			}, 500);
+		} else {
+		$(this).addClass("open");
+			$("nav").css("right", "0");
+			setTimeout(function() {
+				$("#nav-hamburger").removeClass("open");
+				$("#nav-hamburger").addClass("animcomplete");
+			}, 500);
+		}
+	});
+	// End hamburger menu //
