643aba2ca2da8f9ab7dc1ffc6a69f425efb2d38e
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-02-05 03:52:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,8 @@ window.onload = function() {
         }
     });
     $("#nextbuttonP1").click(function() {
-        
+        $(".fade").removeClass("fadein");
+        $(".fade").addClass("done");
         if ($("#lamp").hasClass("shake")) {
             $("#lamp").removeClass("shake");
         } else {
